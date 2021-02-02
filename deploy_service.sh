850712e444d787cf47443139ed9a40f93aa82498
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2021-02-02 15:31:43 +0100

    Hide host name (the one in the previous commits is not available anymore)

--- deploy_service.sh
+++ deploy_service.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+if [[ -z "${CRYSTAL_BALL_HOST}" ]]; then
+  echo 'Missing host name. Set the CRYSTAL_BALL_HOST env var'
+  exit 1
+else
+  CRYSTAL_BALL_HOST="${CRYSTAL_BALL_HOST}"
+fi
+
 yarn build:service
 docker build -t gobbees/crystal-ball:latest .
 docker push gobbees/crystal-ball:latest 
-ssh root@206.189.19.164 "docker pull gobbees/crystal-ball:latest && docker tag gobbees/crystal-ball:latest dokku/crystal-ball:latest && dokku tags:deploy crystal-ball latest"
+ssh root@${CRYSTAL_BALL_HOST} "docker pull gobbees/crystal-ball:latest && docker tag gobbees/crystal-ball:latest dokku/crystal-ball:latest && dokku tags:deploy crystal-ball latest"
